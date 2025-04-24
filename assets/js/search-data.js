1b6d685689e4a1127feb3813b90291955196eb69
Coaxecva <tmquang@gmail.com>
2025-04-24 20:23:25 +0000

    Deploying to gh-pages from @ Coaxecva/coaxecva.github.io@e91a04c1328c0591d097537d81c24288a02aaf70 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,35 +501,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%71%6D%74%72%61%6E@%6D%64%61%6E%64%65%72%73%6F%6E.%6F%72%67", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/quang-tran-9a9bb8249", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=ZCBqwVcAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
