47eb9ed7183a99391e92b7a2a9ce68b6e2d780eb
Coaxecva <tmquang@gmail.com>
2025-09-17 16:36:10 +0000

    Deploying to gh-pages from @ Coaxecva/coaxecva.github.io@e02fd5895f8f2065be1350b255f1271e51f437d6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -31,7 +31,7 @@ ninja.data = [{
             window.location.href = "/cv/";
           },
         },{id: "nav-people",
-          title: "people",
+          title: "People",
           description: "Lab members",
           section: "Navigation",
           handler: () => {
